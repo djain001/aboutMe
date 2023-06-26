86554f709befcfd54835c0d4221674839ecf275b
Diva Jain <divamakesmusic@gmail.com>
2023-06-26 09:43:30 -0400

    Initial Commit

---
+++ About me 3.0/About_me_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  About_me_3_0App.swift
+//  About me 3.0
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct About_me_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
